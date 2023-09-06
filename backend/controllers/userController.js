48f9abc96b462c67243dbeba4102929c8e3c5aa0
mark91 <mark.mekhayl@gmail.com>
2023-09-06 10:53:52 +1000

    added routes & controllers for users

---
+++ backend/controllers/userController.js
@@ -0,0 +1,48 @@
+import asyncHandler from 'express-async-handler'
+
+// Auth user/set token
+// route POST /api/users/auth
+// Public access
+const authUser = asyncHandler(async(req, res) => {
+  res.status(200).json({ message: 'Auth User'});
+});
+
+// Register User
+// route POST /api/users/
+// Public access
+const registerUser = asyncHandler(async(req, res) => {
+  res.status(200).json({ message: 'Register user'});
+});
+
+// Logout User
+// route POST /api/users/logout
+// Public access
+const logoutUser = asyncHandler(async(req, res) => {
+  res.status(200).json({ message: 'Logout user'});
+});
+
+// Get user profile
+// route GET /api/users/profile
+// Private 
+const getUserProfile = asyncHandler(async(req, res) => {
+  res.status(200).json({ message: 'User profile'});
+});
+
+// Update user profile
+// route PUT /api/users/profile
+// Private 
+const updateUserProfile = asyncHandler(async(req, res) => {
+  res.status(200).json({ message: 'Update user profile'});
+});
+
+
+
+
+
+export  {  
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile
+};
