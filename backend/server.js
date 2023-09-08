ee373b63ec35dbbefdbd271c2f2839b6258e6c06
mark91 <mark.mekhayl@gmail.com>
2023-09-08 10:38:32 +1000

    started front-end

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,15 @@ dotenv.config();
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB  from './config/db.js';
+import userRoutes from './routes/userRoutes.js'
 
 const port = process.env.PORT || 5000;
-import userRoutes from './routes/userRoutes.js'
 
 connectDB();
 
 const app = express();
 
+//middleware to handle post and put requests. 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,7 +23,7 @@ app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));
 
-// For the middleware to be in use
+// For the middleware to be in use from middleware folder
 app.use(notFound);
 app.use(errorHandler);
 
